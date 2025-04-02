e9c49ca97a6c3673a11e4e6cd604ac8e23ce5ef7
Manoj Kumar <102353911+manojkr1293@users.noreply.github.com>
2025-04-02 11:42:49 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  output: "export",
+  async redirects() {
+    return [
+      {
+        source: "/admin-dashboard",
+        has: [
+          {
+            type: "cookie",
+            key: "next-auth.session-token",
+            value: "(.*)",
+          },
+        ],
+        permanent: false,
+        destination: "/login",
+      },
+    ];
+  },
+};
 export default nextConfig;
+//module.exports = nextConfig;
